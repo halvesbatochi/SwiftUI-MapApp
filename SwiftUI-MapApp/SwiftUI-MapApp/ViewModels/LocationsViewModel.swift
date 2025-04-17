4913a4d7c78269d4efadb4f35ad8edbab07ae33d
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2025-04-17 00:16:53 -0300

    Location Preview cards with asynmetric transitions

--- SwiftUI-MapApp/SwiftUI-MapApp/ViewModels/LocationsViewModel.swift
+++ SwiftUI-MapApp/SwiftUI-MapApp/ViewModels/LocationsViewModel.swift
@@ -54,4 +54,27 @@ class LocationsViewModel: ObservableObject {
             showLocationsList = false
         }
     }
+    
+    func nextButtonPressed() {
+        
+        // Get the current location
+        guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+            print("Could not find current index in locations array! Should never happen.")
+            return
+        }
+        
+        // Check if the currentIndex is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            // Next index is NOT valid
+            // Restart from 0
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        // Next index IS valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+    }
 }
